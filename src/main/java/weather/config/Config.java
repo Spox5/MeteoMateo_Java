5fa94ef591d78aeb7314694c5638d5236129cebb
Mateusz Musiela <mateusz.musiela@gmail.com>
2020-08-12 10:36:01 +0200

    Delete unnecessary database

--- src/main/java/weather/config/Config.java
+++ src/main/java/weather/config/Config.java
@@ -4,5 +4,4 @@ public class Config {
 
     public final static String API_KEY = "80c244e24e82bfead796cf62fb799a73";
     public final static String CITY_LIST_WITH_DATA = "/cities/city.list.min.json";
-    public final static String CITIES_NAMES = "/cities/city_names.json";
 }
